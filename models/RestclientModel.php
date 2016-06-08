95d2a050b7cb59e2b6782b3dc5da2ef5b09b49af
Paolo Melo <paolorbmelo@gmail.com>
2016-06-08 15:51:26 -0300

    metodo para retornar dados salvos no BD

--- models/RestclientModel.php
+++ models/RestclientModel.php
@@ -2,5 +2,14 @@
 	class RestclientModel extends Model{
 		public $tabela = "sintegra";
 		public $campo_id = "id";
+
+		public function retornaConsultas($id){
+			$query = "SELECT * FROM " . $this->tabela . " WHERE idusuario = " . $id;
+			$rs = $this->db->prepare($query);
+			$rs->execute(array());
+			$this->consultas = $rs->fetchAll(PDO::FETCH_ASSOC);
+
+			return $this->consultas;
+		}
 	}
 ?>
